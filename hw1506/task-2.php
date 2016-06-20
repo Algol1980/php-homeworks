94e264f9c2c4f0b28e83364fed89ee8bbe326208
algol1980 <algol1980@yandex.ru>
2016-06-20 13:36:12 +0300

    Task 2 done

---
+++ hw1506/task-2.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Алексей
+ * Date: 20.06.2016
+ * Time: 13:24
+ */
+
+if (isset($_POST['task2']) && !empty($_POST['text'])) {
+    $pattern = '/([:|;]{1}-*(\({1,}|\){1,}|\[{1,}|\]{1,}))/';
+    preg_match_all($pattern, $_POST['text'], $matches);
+    if (!empty($matches)) {
+        $result = count($matches[1]);
+    }
+    else {
+        $result = 'Нет смайликов';
+    }
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="post" enctype="multipart/form-data">
+    <label for="text">Введите текст</label>
+    <textarea name="text" id="" cols="30" rows="10"></textarea>
+    <input type="submit" name="task2">
+</form>
+<p><?php echo isset($result) ?  'Смайликов: ' . $result : ''; ?></p>
+</body>
+</html>
